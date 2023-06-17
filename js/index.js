d07446658618f1a2e5968fb02b188a419f7c39a6
dannhu283 <dannhu2803@gmail.com>
2023-06-17 22:18:21 +0700

    add function main and bt1

--- js/index.js
+++ js/index.js
@@ -1,6 +1,23 @@
+//hàm để user nhập giá trị vào tính toán
+let numArray = [];
+//hàm thêm số vào mảng
 function showNumber() {
   let number = +document.getElementById("number").value;
-  let numArray = [];
   numArray.push(number);
   document.getElementById("result").innerHTML = numArray;
+}
+//hàm xóa số vừa nhập khỏi mảng
+function removeNumber() {
+  numArray.pop();
+  document.getElementById("result").innerHTML = numArray;
+}
+//hàm tính tổng số dương của bt1
+function sumPositive() {
+  let sumPositive = 0;
+  for (let i = 0; i < numArray.length; i++) {
+    if (numArray[i] > 0) {
+      sumPositive += numArray[i];
+    }
+  }
+  document.getElementById("sum").innerHTML = `Tổng số dương : ${sumPositive}`;
 }
