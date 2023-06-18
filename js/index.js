34389852de3f738d3fc82276aee73c0073e4a1a8
dannhu283 <dannhu2803@gmail.com>
2023-06-18 23:55:52 +0700

    add bt 4 5 7

--- js/index.js
+++ js/index.js
@@ -31,12 +31,56 @@ function countPositive() {
   }
   document.getElementById("count").innerHTML = ` Số dương: ${count}`;
 }
-// hàm tìm số nhỏ nhất
+// hàm tìm số nhỏ nhất bt3
+//hàm để đưa vào sort để săp xếp giá trị từ thấp đến cao
 function compare(a, b) {
   return a - b;
 }
 function smallestNumber() {
   numArray.sort(compare);
+  //lấy giá trị đầu tiên do trước đó đã sort
   let smNumber = numArray[0];
   document.getElementById("smNumber").innerHTML = `Số nhỏ nhất: ${smNumber}`;
 }
+//hàm tìm số dương nhỏ nhất bt 4
+function smallestPositive() {
+  let newArray = [];
+  for (let i = 0; i <= numArray.length; i++) {
+    if (numArray[i] > 0) {
+      newArray.push(numArray[i]);
+    }
+  }
+  if (newArray.length > 0) {
+    newArray.sort(compare);
+    let smPositive = newArray[0];
+    document.getElementById(
+      "smPositive"
+    ).innerHTML = `Số dương nhỏ nhất: ${smPositive}`;
+  } else {
+    document.getElementById(
+      "smPositive"
+    ).innerHTML = `Không có số dương trong mảng`;
+  }
+}
+
+//hàm tìm số chẵn cuối cùng bt5
+function lastEven() {
+  let numberEven = 0;
+  for (let i = 0; i <= numArray.length; i++) {
+    if (numArray[i] % 2 === 0) {
+      numberEven = numArray[i];
+    }
+  }
+  document.getElementById(
+    "numberEven"
+  ).innerHTML = `Số chẵn cuối cùng: ${numberEven}`;
+}
+//bt7
+function sort() {
+  numArray.sort(compare);
+  document.getElementById(
+    "sortAscending"
+  ).innerHTML = `Mảng sau khi sắp xếp : ${numArray}`;
+}
+// //hàm tìm số nguyên tố đầu tiên bt8
+//*************còn 6 8 9 10 và mở public
